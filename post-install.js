5a9c5411dda9db47a1dd0200dc31727ff6254e0d
Alexander Behrens <alexander.behrens@speakap.nl>
2023-03-17 11:29:53 +0100

    updating paths in postinstall

--- post-install.js
+++ post-install.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
+fs.writeFileSync("alex.txt", process.env.INIT_CWD);
+
 if (!fs.existsSync(path.resolve(process.env.INIT_CWD, "./tsconfig.json"))) {
     fs.copyFileSync(path.resolve(process.env.INIT_CWD, "./node_modules/@taskli-st/tsconfig/tsconfig-client.json"), path.resolve(process.env.INIT_CWD, "./tsconfig.json"));
 }
