d48bbf1b5c0ffa9ed4404a0aaf4d7156f1378b00
Alexander Behrens <alexander.behrens@speakap.nl>
2023-03-17 09:59:45 +0100

    updating paths in postinstall

--- post-install.js
+++ post-install.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 
-if (!fs.existsSync(path.resolve(__dirname, "./tsconfig.json"))) {
-    fs.copyFileSync(path.resolve(__dirname, "./node_modules/@taskli-st/tsconfig/tsconfig-client.json"), path.resolve(__dirname, "./tsconfig.json"));
+if (!fs.existsSync(path.resolve(__dirname, "../../../tsconfig.json"))) {
+    fs.copyFileSync(path.resolve(__dirname, "./tsconfig-client.json"), path.resolve(__dirname, "../../../tsconfig.json"));
 }
