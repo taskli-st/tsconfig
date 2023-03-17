be79dc21e6a820fa95eff02dd3c732d7d6ea7f3b
Alexander Behrens <alexander.behrens@speakap.nl>
2023-03-17 09:54:42 +0100

    updating postinstall

--- post-install.js
+++ post-install.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
+const path = require("path");
 
-if (!fs.existsSync("./tsconfig.json")) {
-    fs.copyFileSync("./node_modules/@taskli-st/tsconfig/tsconfig-client.json", "./tsconfig.json");
+if (!fs.existsSync(path.resolve(__dirname, "./tsconfig.json"))) {
+    fs.copyFileSync(path.resolve(__dirname, "./node_modules/@taskli-st/tsconfig/tsconfig-client.json"), path.resolve(__dirname, "./tsconfig.json"));
 }
